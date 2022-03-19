da3d433b2d684a71e0e6446d2d4fe473a85b8414
gustavodiniz <gustavo.diniz@tangerino.com.br>
2022-03-19 14:55:09 -0300

    testing calculator.component.spec.ts

--- src/app/calculator/components/calculator.component.spec.ts
+++ src/app/calculator/components/calculator.component.spec.ts
@@ -1,4 +1,5 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import {CalculatorComponent} from './calculator.component';
 import {CalculatorService} from "../services";
@@ -23,5 +24,27 @@ describe('CalculatorComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('must ensure that 3 + 2 = 5', () => {
+    let btn3 = fixture.debugElement.query(By.css('#btn3'));
+    let btnSum = fixture.debugElement.query(By.css('#btnSoma'));
+    let btn2 = fixture.debugElement.query(By.css('#btn2'));
+    let btnCalculate = fixture.debugElement.query(By.css('#btnCalcular'));
+    let display = fixture.debugElement.query(By.css('#display'));
+
+    btn3.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    btnSum.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    btn2.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    btnCalculate.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    expect(display.nativeElement.value).toEqual('5');
   });
 });
