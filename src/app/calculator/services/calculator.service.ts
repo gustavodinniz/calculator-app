96d4cc0c1919d6567092b34f1ecae4e3cf4ca96c
gustavodiniz <gustavo.diniz@tangerino.com.br>
2022-03-19 12:58:07 -0300

    implementing service in calculator service

--- src/app/calculator/services/calculator.service.ts
+++ src/app/calculator/services/calculator.service.ts
@@ -1,9 +1,38 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CalculatorService {
 
-  constructor() { }
+  static readonly SUM: string = '+';
+  static readonly SUBTRACTION: string = '-';
+  static readonly DIVISION: string = '/';
+  static readonly MULTIPLICATION: string = '*';
+
+  constructor() {
+  }
+
+  calculate(num1: number, num2: number, operation: string): number {
+    let result: number; // armazena o result da operação
+
+    switch (operation) {
+      case CalculatorService.SUM:
+        result = num1 + num2;
+        break;
+      case CalculatorService.SUBTRACTION:
+        result = num1 - num2;
+        break;
+      case CalculatorService.DIVISION:
+        result = num1 / num2;
+        break;
+      case CalculatorService.MULTIPLICATION:
+        result = num1 * num2;
+        break;
+      default:
+        result = 0;
+    }
+
+    return result;
+  }
 }
